647efb2078b8ef2b62173fcdac93add990a7ccc3
Nanno <gdzelishviliv@gmail.com>
2025-09-05 00:59:41 +0400

    feat[add]-first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <>
+    <div className='flex justify-between px-[20px] py-[12px] bg-[#132450]'>
+        <div>logo</div>
+        <button>
+            x
+        </button>
+    </div>
+    </>
+  )
+}
