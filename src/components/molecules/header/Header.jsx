60cf72538119db6c72fd74de2370acc4d4b586f9
Nanno <gdzelishviliv@gmail.com>
2025-09-05 01:18:00 +0400

    feat[add]-header resp on tablet screen

--- src/components/molecules/header/Header.jsx
+++ src/components/molecules/header/Header.jsx
@@ -1,10 +1,10 @@
-import BurgerMenuIcn from "../../assets/menu.svg";
-import MainLogo from "../../assets/main-logo.svg";
+import BurgerMenuIcn from "../../../assets/menu.svg";
+import MainLogo from "../../../assets/main-logo.svg";
 
 export const Header = () => {
   return (
     <>
-      <div className="flex justify-between px-[20px] py-[12px] bg-[#132450] h-[60px]">
+      <div className="flex md:px-[30px] justify-between px-[20px] py-[12px] bg-[#132450] h-[60px] transition-all">
         <div className="flex gap-[9.5px] items-center cursor-pointer">
           <img src={MainLogo} alt="Main Logo" width={34} height={36} />
           <h2 className="text-white font-bold cu">Enagram</h2>
