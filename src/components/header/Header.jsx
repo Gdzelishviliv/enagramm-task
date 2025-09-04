2500eb3a4eab5df43e40b044c8bb948bba6e1fb4
Nanno <gdzelishviliv@gmail.com>
2025-09-05 01:12:49 +0400

    feat[add]-header section on mobile first resp

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import BurgerMenuIcn from "../../assets/menu.svg";
+import MainLogo from "../../assets/main-logo.svg";
 
 export const Header = () => {
   return (
     <>
-    <div className='flex justify-between px-[20px] py-[12px] bg-[#132450]'>
-        <div>logo</div>
-        <button>
-            x
+      <div className="flex justify-between px-[20px] py-[12px] bg-[#132450] h-[60px]">
+        <div className="flex gap-[9.5px] items-center cursor-pointer">
+          <img src={MainLogo} alt="Main Logo" width={34} height={36} />
+          <h2 className="text-white font-bold cu">Enagram</h2>
+        </div>
+        <button className="cursor-pointer">
+          <img src={BurgerMenuIcn} alt="Burger Menu" />
         </button>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
